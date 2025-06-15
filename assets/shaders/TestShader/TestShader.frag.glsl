62249387c71d92464a2e76671d78f627935bb3f7
ElectronPie <tima001f@gmail.com>
2025-06-15 11:34:38 +0300

    Now using HSV in polar coordinates instad of cartesian

--- assets/shaders/TestShader/TestShader.frag.glsl
+++ assets/shaders/TestShader/TestShader.frag.glsl
@@ -8,17 +8,23 @@ uniform float u_time;
 uniform vec2 u_mouse;
 uniform vec2 u_resolution;
 
+#include "Constants.glsl"
 #include "HSV.glsl"
 
 void main()
 {
     vec2 st = gl_FragCoord.xy / u_resolution;
-
     vec3 color = vec3(0.0);
 
-    // We map x (0.0 - 1.0) to the hue (0.0 - 1.0)
-    // And the y (0.0 - 1.0) to the brightness
-    color = HSV2RGB(vec3(st.x, 1.0, st.y));
+    // Use polar coordinates instead of cartesian
+    vec2 toCenter = vec2(0.5) - st;
+    float angle = atan(toCenter.y, toCenter.x);
+    angle = mod(angle+u_time, TWO_PI);
+    float radius = length(toCenter);
+
+    // Map the angle (-PI to PI) to the Hue (from 0 to 1)
+    // and the Saturation to the radius
+    color = HSV2RGB(vec3(angle/TWO_PI, 2.0*radius, 1.0));
 
     FragColor = vec4(color, 1.0);
 }
