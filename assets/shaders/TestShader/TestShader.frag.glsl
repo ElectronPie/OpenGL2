90aa4320cee494fb188591a5ad1b0d2122622c1a
ElectronPie <tima001f@gmail.com>
2025-06-15 12:42:17 +0300

    Made the color wheel look like a RYB color picker wheel

--- assets/shaders/TestShader/TestShader.frag.glsl
+++ assets/shaders/TestShader/TestShader.frag.glsl
@@ -11,6 +11,13 @@ uniform vec2 u_resolution;
 #include "Constants.glsl"
 #include "HSV.glsl"
 
+float shaping(in float h)
+{
+    return h <= 0.5 ?
+        2.0/3.0*h :
+        4.0/3.0*h - 1.0/3.0;
+}
+
 void main()
 {
     vec2 st = gl_FragCoord.xy / u_resolution;
@@ -21,10 +28,11 @@ void main()
     float angle = atan(toCenter.y, toCenter.x);
     angle = mod(angle+u_time, TWO_PI);
     float radius = length(toCenter);
+    radius = radius >= 0.5 ? 0.0 : radius;
 
     // Map the angle (-PI to PI) to the Hue (from 0 to 1)
     // and the Saturation to the radius
-    color = HSV2RGB(vec3(angle/TWO_PI, 2.0*radius, 1.0));
+    color = HSV2RGB(vec3(shaping(angle/TWO_PI), 2.0*radius, 1.0));
 
     FragColor = vec4(color, 1.0);
 }
