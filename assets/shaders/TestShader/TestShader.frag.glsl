8a77c9d84e84dda2fd3cb24c92170b1a65139a1d
ElectronPie <tima001f@gmail.com>
2025-06-15 11:16:35 +0300

    Added HSV-RGB conversion functions

--- assets/shaders/TestShader/TestShader.frag.glsl
+++ assets/shaders/TestShader/TestShader.frag.glsl
@@ -8,35 +8,17 @@ uniform float u_time;
 uniform vec2 u_mouse;
 uniform vec2 u_resolution;
 
-#include "Easings.glsl"
-
-vec3 colorA = vec3(0.129,0.336,0.184);
-vec3 colorB = vec3(0.863,0.605,0.164);
-
-float plot(vec2 st, float pct)
-{
-    return smoothstep(pct - 0.01, pct, st.y) - smoothstep(pct, pct + 0.01, st.y);
-}
+#include "HSV.glsl"
 
 void main()
 {
     vec2 st = gl_FragCoord.xy / u_resolution;
 
-    float y;
-    vec3 color = vec3(0);
+    vec3 color = vec3(0.0);
 
-    vec3 pct = vec3(st.x);
-
-    pct.r = smoothstep(0.0,1.0, st.x);
-    pct.g = sin(st.x*PI);
-    pct.b = pow(st.x,0.5);
-
-    color = mix(colorA, colorB, pct);
-
-    // Plot transition lines for each channel
-    color = mix(color,vec3(1.0,0.0,0.0),plot(st,pct.r));
-    color = mix(color,vec3(0.0,1.0,0.0),plot(st,pct.g));
-    color = mix(color,vec3(0.0,0.0,1.0),plot(st,pct.b));
+    // We map x (0.0 - 1.0) to the hue (0.0 - 1.0)
+    // And the y (0.0 - 1.0) to the brightness
+    color = HSV2RGB(vec3(st.x, 1.0, st.y));
 
     FragColor = vec4(color, 1.0);
 }
